2f50356c2b40d81f8d9d78844f93e3d23799913c
hernanjhc <hernan.jhc@gmail.com>
2021-09-02 20:47:17 -0300

    Configurar Autenticación.

--- PeliculasWeb/Startup.cs
+++ PeliculasWeb/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,17 @@ namespace PeliculasWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Agregamos autenticacion. JWT se guarda en cokie del navegador
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options => {
+                    options.Cookie.HttpOnly = true;
+                    //tiempo de duracion de cokie
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                    options.LoginPath = "/Home/Login";
+                    options.AccessDeniedPath = "/Home/AccessDenied";
+                    options.SlidingExpiration = true;
+                });
+
             services.AddControllersWithViews();
 
             //Llamados http
@@ -38,6 +50,14 @@ namespace PeliculasWeb
             services.AddScoped<IPeliculaRepository, PeliculaRepository>();
             services.AddScoped<IUsuarioRepository, UsuarioRepository>();
             services.AddScoped<IAccountRepository, AccountRepository>();
+
+            //configurar sesion
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(10);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -58,6 +78,8 @@ namespace PeliculasWeb
 
             app.UseRouting();
 
+            app.UseSession();
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
